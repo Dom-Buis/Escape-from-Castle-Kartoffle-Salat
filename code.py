e6bbc9ef655b5fb311e8cd41eac08552071a442b
Dom-Buis <23020@bayfield-high.school.nz>
2026-07-21 14:43:07 +1200

    backstory added

--- code.py
+++ code.py
@@ -1,1 +1,2 @@
 #print original text/backstory
+print('You were on a hike in the alps, you felt the cold snow beneath your boots, you were doing well but then you slipped, hit your head on the ice, and fell unconscious. Now youve woken up here, the bedroom of a strange manor')
