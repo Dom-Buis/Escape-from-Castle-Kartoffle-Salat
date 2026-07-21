e733f76dfe3ee7ff977330ffca6052880c7a746c
Dom-Buis <23020@bayfield-high.school.nz>
2026-07-21 14:59:37 +1200

    bedroom code added

--- code.py
+++ code.py
@@ -1,2 +1,10 @@
 #print original text/backstory
 print('You were on a hike in the alps. \n you felt the cold snow beneath your boots, you were doing well but then you slipped, hit your head on the ice, and fell unconscious. \n Now youve woken up here, the bedroom of a strange manor')
+
+#bedroom
+have_key = 'false'
+print('You are locked inside an aristocratic bedroom, theres a king sized bed with a duvet, and theres two comfy looking cushons. You feel like you could sleep here')
+if have_key == 'false'
+  print('Now, lets try find a key,)
+if have_key == 'true'
+  print('Alright, its time to go')
