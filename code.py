1040d2e3bb77a60f5da83c74ebdee336e2f4dca4
Lochlann-01 <23105@bayfield-high.school.nz>
2026-07-23 10:27:31 +1200

    improve grammer

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 #print original text/backstory
-print('You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well but then you slipped, hit your head on the ice, and fell unconscious. \n Now you've woken up here, the bedroom of a strange manor.')
+print('You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, the bedroom of a strange manor.')
 
 #first room
 have_bedroom_key = 'false'
-print('You are locked inside an aristocratic bedroom, theres a king size bed with a duvet, and theres two comfy looking cushons, you feel like you could sleep here')
+print('You are locked inside an aristocratic bedroom, there's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, you feel like you could sleep here. ')
 if have_bedroom_key == 'false':
   print('Now, lets find that key')
 if have_bedroom_key == 'true':
