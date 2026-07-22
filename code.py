3ddf7ffd5115e4178b72f45cebf5ac2c063f9773
Dom-Buis <23020@bayfield-high.school.nz>
2026-07-23 10:31:50 +1200

    added blank line

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 #print original text/backstory
 print("You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, in the bedroom of a strange manor.")
-
+print()
 #first room
 have_bedroom_key = 'false'
 print("You are locked inside an aristocratic bedroom, there's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, you feel like you could sleep here. ")
