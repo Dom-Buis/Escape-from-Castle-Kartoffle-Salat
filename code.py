2a0afc913fd391dfaf73e1195169f845b55989b3
Lochlann-01 <23105@bayfield-high.school.nz>
2026-07-23 10:31:09 +1200

    updated to use double quotes

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
 #print original text/backstory
-print('You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, in the bedroom of a strange manor.')
+print("You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, in the bedroom of a strange manor.")
 
 #first room
 have_bedroom_key = 'false'
-print('You are locked inside an aristocratic bedroom, there's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, you feel like you could sleep here. ')
+print("You are locked inside an aristocratic bedroom, there's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, you feel like you could sleep here. ")
 if have_bedroom_key == 'false':
-  print('Now, lets find that key')
+  print("Now, lets find that key")
 if have_bedroom_key == 'true':
-  print('Alright, it's time to go')
+  print("Alright, it's time to go")
