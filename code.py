b37f9493a79ec4b1a9ffcde134ea27ebcc43dd50
Lochlann-01 <23105@bayfield-high.school.nz>
2026-07-23 10:29:02 +1200

    fix

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 #print original text/backstory
-print('You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, the bedroom of a strange manor.')
+print('You were on a hike in the alps. \n You felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \n Now you've woken up here, in the bedroom of a strange manor.')
 
 #first room
 have_bedroom_key = 'false'
