f3e982a6a383c542e4645e98ee1c0dc86b6e675e
Lochlann-01 <23105@bayfield-high.school.nz>
2026-08-11 13:58:08 +1200

    started work on refining prompts.

--- code.py
+++ code.py
@@ -22,12 +22,12 @@ def typing2(text, delay=0.07):
         time.sleep(delay)
 
 #print original text/backstory
-typing("You were on a hike in the alps. \nYou felt the cold snow beneath your boots, you were doing well on the hike, but then you slipped and hit your head on the ice, and you fell unconscious. \nNow you've woken up here, in the bedroom of a strange manor.")
+typing("You were on a hike in the alps. You felt the cold snow beneath your boots, \nyou were doing well on the hike, but then you slipped and hit your head on the ice......  \nYou fell unconscious. \nNow you've woken up here, in the bedroom of a strange manor.")
 print()
 
 #first room
 have_bedroom_key = 'false'
-typing2("You are locked inside an aristocratic bedroom, there's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, you feel like you could sleep here...\n ")
+typing2("You are locked inside an aristocratic bedroom, \nthere's a king size bed next to you with a duvet on it, and theres two comfy looking cushions, \nyou feel like you could sleep here...\n")
 def bedroom_key():
   if have_bedroom_key == 'false':
     typing2("'Now, lets find that key'")
@@ -37,16 +37,16 @@ def bedroom_key():
 #code to escape the first room
 typing2("What do you do?\n")
 while True:
-  search = input(f"{INPUT_COLOUR}Do you try the door or do you search the room?{RESET} ").upper()
-  if search == "SEARCH THE ROOM":
+  search = input(f"{INPUT_COLOUR}Do you try the door or do you search the room? \nTo try the door type 'DOOR', and to search the room type 'ROOM'.{RESET} ").upper()
+  if search == "ROOM":
     while True:
         #code to explore the first room
-        where = input(f"{INPUT_COLOUR}Where in the room do you check?\n There are 3 spots, there's a pillowcase, there's the vase, and there is behind the curtain.{RESET} ").upper()
-        if where == "BEHIND THE CURTAIN":
+        where = input(f"{INPUT_COLOUR}Where in the room do you check? \nThere are 3 spots, there's a pillowcase, there's the vase, and there is behind the curtain. \nTo check the pillowcase type 'PILLOWCASE'. to check the vase type 'VASE', \nand to check behind the curtain type 'CURTAIN'.{RESET} ").upper()
+        if where == "CURTAIN":
           typing("Wrong place, try again.\n")
-        elif where == "THE VASE":
+        elif where == "VASE":
           typing("Wrong place, try again.\n")
-        elif where == "A PILLOWCASE":
+        elif where == "PILLOWCASE":
           typing("You checked the pillowcase, and you found the key.\n")
           #get bedroom key
           have_bedroom_key = 'true'
@@ -54,7 +54,7 @@ while True:
         else:
           print("that isn't a valid option")
           #open the door
-  elif search == "TRY THE DOOR":
+  elif search == "DOOR":
       typing2("The door won't budge.\n")
       if have_bedroom_key == 'true':
         key_used = input(f"{INPUT_COLOUR}Try the key?{RESET} ").upper()
