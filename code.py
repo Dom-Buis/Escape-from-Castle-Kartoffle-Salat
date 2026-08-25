818e6fe0fae7dcd3f81b7b91567ec6ddd56d65c3
Dom-Buis <23020@bayfield-high.school.nz>
2026-08-25 14:59:32 +1200

    Enhance game start and settings input validation
    Added input validation for game selection and improved user prompts.

--- code.py
+++ code.py
@@ -8,6 +8,11 @@ start = time.time()
 print('Would you like to start the game or open game settings? (type "game" or "settings")')
 game = input('').upper()
 
+if game != ('GAME') or ('SETTINGS'):
+    print('Please enter a valid option...\n')
+    print('Would you like to start the game or open game settings? (type "game" or "settings")')
+    game = input('').upper()
+
 if game == ('GAME'):
     #font colour change for input text
     INPUT_COLOUR = '\033[90m'
@@ -111,6 +116,10 @@ elif game == ('SETTINGS'):
     if input_text_colour_choice == ('PINK'):
         INPUT_COLOUR = '\033[95m'
         
+#else:
+   # print('Please enter a valid option...')
+ #   print('Would you like to start the game or open game settings? (type "game" or "settings")')
+#game = input('').upper()
 
 
 
@@ -230,4 +239,3 @@ num = length
 num1 = num/60
 time1 = round(num1, 1)
 print("Program ran for", time1, "minutes")
-
